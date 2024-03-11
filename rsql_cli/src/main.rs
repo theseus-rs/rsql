ef7f803c0cd6d11f4b60b3ff05e63ffc09e08b27
brianheineman <brian.heineman@gmail.com>
2024-03-10 19:18:37 -0600

    test: add cli tests

--- rsql_cli/src/main.rs
+++ rsql_cli/src/main.rs
@@ -10,7 +10,7 @@ use std::io;
 use tracing::info;
 
 #[derive(Debug, Parser)]
-struct Args {
+pub(crate) struct Args {
     /// The shell arguments
     #[clap(flatten)]
     pub shell_args: ShellArgs,
@@ -24,6 +24,11 @@ struct Args {
 async fn main() -> Result<()> {
     let _ = dotenvy::dotenv();
     let args = Args::try_parse()?;
+
+    execute(&args, &mut io::stdout()).await
+}
+
+pub(crate) async fn execute(args: &Args, output: &mut dyn io::Write) -> Result<()> {
     let program_name = "rsql";
     let version = env!("CARGO_PKG_VERSION");
     let mut configuration = ConfigurationBuilder::new(program_name, version)
@@ -34,11 +39,39 @@ async fn main() -> Result<()> {
     info!("{version} initialized");
 
     let result = if args.version {
-        version::execute(&mut configuration, &mut io::stdout()).await
+        version::execute(&mut configuration, output).await
     } else {
         shell::execute(&mut configuration, &args.shell_args).await
     };
 
     info!("{version} completed");
     result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_main() {
+        let result = main();
+
+        assert!(result.is_err());
+    }
+
+    #[tokio::test]
+    async fn test_execute_version() -> Result<()> {
+        let args = Args {
+            shell_args: ShellArgs::default(),
+            version: true,
+        };
+
+        let mut output = Vec::new();
+        let result = execute(&args, &mut output).await;
+
+        assert!(result.is_ok());
+        let version = String::from_utf8(output)?;
+        assert!(version.starts_with("rsql/"));
+        Ok(())
+    }
 }
