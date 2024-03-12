cd04927cbae9393a074c6e92c871fcfdbec8f75f
brianheineman <brian.heineman@gmail.com>
2024-03-12 11:11:48 -0600

    internal: disable sqlite as default database url when the sqlite feature is not enabled

--- rsql_core/src/shell/mod.rs
+++ rsql_core/src/shell/mod.rs
@@ -18,17 +18,34 @@ use clap::Parser;
 pub use crate::shell::command::Commands;
 pub use repl::execute;
 
+#[cfg(feature = "sqlite")]
+const DEFAULT_URL: &str = "sqlite::memory:";
+
+#[cfg(not(feature = "sqlite"))]
+const DEFAULT_URL: &str = "";
+
 #[derive(Debug, Parser)]
 pub struct ShellArgs {
     /// The url of the database
-    #[arg(long, default_value = "sqlite::memory:", env = "DATABASE_URL")]
+    #[arg(long, default_value = DEFAULT_URL, env = "DATABASE_URL")]
     pub url: String,
 }
 
 impl Default for ShellArgs {
     fn default() -> Self {
         ShellArgs {
-            url: "sqlite::memory:".to_string(),
+            url: DEFAULT_URL.to_string(),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_default() {
+        let args = ShellArgs::default();
+        assert_eq!(args.url, DEFAULT_URL);
     }
 }
