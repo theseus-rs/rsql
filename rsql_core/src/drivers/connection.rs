79bec3561fc760ea2f438b6971cb416ffea0387c
brianheineman <brian.heineman@gmail.com>
2024-03-13 17:38:51 -0600

    refactor!: refactore results to support configurable output formatting

--- rsql_core/src/drivers/connection.rs
+++ rsql_core/src/drivers/connection.rs
@@ -1,6 +1,10 @@
 use crate::drivers::value::Value;
 use async_trait::async_trait;
 
+pub enum Results {
+    Query(QueryResult),
+    Execute(u64),
+}
 pub struct QueryResult {
     pub(crate) columns: Vec<String>,
     pub(crate) rows: Vec<Vec<Option<Value>>>,
@@ -9,8 +13,8 @@ pub struct QueryResult {
 #[cfg_attr(test, mockall::automock)]
 #[async_trait]
 pub trait Connection: Send {
-    async fn execute(&self, sql: &str) -> anyhow::Result<u64>;
-    async fn query(&self, sql: &str) -> anyhow::Result<QueryResult>;
+    async fn execute(&self, sql: &str) -> anyhow::Result<Results>;
+    async fn query(&self, sql: &str) -> anyhow::Result<Results>;
     async fn tables(&mut self) -> anyhow::Result<Vec<String>>;
     async fn stop(&mut self) -> anyhow::Result<()>;
 }
