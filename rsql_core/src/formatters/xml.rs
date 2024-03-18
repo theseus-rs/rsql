238b6f3adf9e00083e53e4c8f23a2721b4f79e8b
brianheineman <brian.heineman@gmail.com>
2024-03-18 14:41:28 -0600

    feat: add xml format

---
+++ rsql_core/src/formatters/xml.rs
@@ -0,0 +1,134 @@
+use crate::drivers::Value;
+use crate::formatters::error::Result;
+use crate::formatters::footer::write_footer;
+use crate::formatters::formatter::FormatterOptions;
+use async_trait::async_trait;
+use quick_xml::events::{BytesEnd, BytesStart, BytesText, Event};
+use quick_xml::Writer;
+
+/// A formatter for XML
+#[derive(Debug, Default)]
+pub struct Formatter;
+
+#[async_trait]
+impl crate::formatters::Formatter for Formatter {
+    fn identifier(&self) -> &'static str {
+        "xml"
+    }
+
+    async fn format<'a>(&self, options: &mut FormatterOptions<'a>) -> Result<()> {
+        format_xml(options).await
+    }
+}
+
+pub(crate) async fn format_xml(options: &mut FormatterOptions<'_>) -> Result<()> {
+    let query_result = match options.results {
+        crate::drivers::Results::Query(query_result) => query_result,
+        _ => return write_footer(options),
+    };
+
+    let mut writer = Writer::new(&mut options.output);
+
+    writer.write_event(Event::Start(BytesStart::new("results")))?;
+    writeln!(writer.get_mut())?;
+
+    let columns: Vec<String> = query_result.columns.iter().map(|c| c.to_string()).collect();
+    for row in &query_result.rows {
+        writer.write_event(Event::Start(BytesStart::new("row")))?;
+
+        for (c, data) in row.iter().enumerate() {
+            let column = columns.get(c).expect("column not found");
+
+            writer.write_event(Event::Start(BytesStart::new(column)))?;
+            match data {
+                Some(value) => {
+                    let string_value = if let Value::Bytes(_bytes) = value {
+                        let value = Value::String(value.to_string());
+                        value.to_string()
+                    } else {
+                        value.to_string()
+                    };
+                    writer.write_event(Event::Text(BytesText::new(string_value.as_str())))?;
+                }
+                None => {}
+            }
+
+            writer.write_event(Event::End(BytesEnd::new(column)))?;
+        }
+
+        writer.write_event(Event::End(BytesEnd::new("row")))?;
+        writeln!(writer.get_mut())?;
+    }
+
+    writer.write_event(Event::End(BytesEnd::new("results")))?;
+    writeln!(options.output)?;
+
+    write_footer(options)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::configuration::Configuration;
+    use crate::drivers::QueryResult;
+    use crate::drivers::Results::{Execute, Query};
+    use crate::drivers::Value;
+    use crate::formatters::formatter::FormatterOptions;
+    use crate::formatters::Formatter;
+    use rustyline::ColorMode;
+    use std::io::Cursor;
+
+    #[tokio::test]
+    async fn test_format_execute() -> anyhow::Result<()> {
+        let configuration = &mut Configuration {
+            color_mode: ColorMode::Disabled,
+            ..Default::default()
+        };
+        let output = &mut Cursor::new(Vec::new());
+        let mut options = FormatterOptions {
+            configuration,
+            results: &Execute(1),
+            elapsed: &std::time::Duration::from_nanos(9),
+            output,
+        };
+
+        let formatter = Formatter;
+        formatter.format(&mut options).await.unwrap();
+
+        let output = String::from_utf8(output.get_ref().to_vec())?.replace("\r\n", "\n");
+        let expected = "1 row (9ns)\n";
+        assert_eq!(output, expected);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_format_query() -> anyhow::Result<()> {
+        let configuration = &mut Configuration {
+            color_mode: ColorMode::Disabled,
+            ..Default::default()
+        };
+        let query_result = Query(QueryResult {
+            columns: vec!["id".to_string(), "data".to_string()],
+            rows: vec![
+                vec![Some(Value::I64(1)), Some(Value::Bytes(b"bytes".to_vec()))],
+                vec![Some(Value::I64(2)), Some(Value::String("foo".to_string()))],
+                vec![Some(Value::I64(3)), None],
+            ],
+        });
+        let output = &mut Cursor::new(Vec::new());
+        let mut options = FormatterOptions {
+            configuration,
+            results: &query_result,
+            elapsed: &std::time::Duration::from_nanos(9),
+            output,
+        };
+
+        let formatter = Formatter;
+        formatter.format(&mut options).await.unwrap();
+
+        let output = String::from_utf8(output.get_ref().to_vec())?.replace("\r\n", "\n");
+        let expected = "<results>\n<row><id>1</id><data>Ynl0ZXM=</data></row>\n<row><id>2</id><data>foo</data></row>\n<row><id>3</id><data></data></row>\n</results>\n3 rows (9ns)\n".to_string();
+        assert_eq!(output, expected);
+        Ok(())
+    }
+}
