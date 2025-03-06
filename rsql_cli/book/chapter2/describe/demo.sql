f8456bf14a2a7980f58d91502c075d0a7ff63863
brianheineman <brian.heineman@gmail.com>
2025-03-06 22:22:25 +0000

    Deploying to gh-pages from @ theseus-rs/rsql@298a5a70db06a94c337de5ad91fe36a38bb2420f 🚀

---
+++ rsql_cli/book/chapter2/describe/demo.sql
@@ -0,0 +1,13 @@
+.echo prompt
+CREATE TABLE users
+(
+    id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE
+);
+
+.echo off
+.sleep 1
+.echo prompt
+.describe users
