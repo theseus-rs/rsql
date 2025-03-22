7a76a76acdfb056b4640b0cb96c39aec63f10a88
brianheineman <brian.heineman@gmail.com>
2025-03-22 03:01:01 +0000

    Deploying to gh-pages from @ theseus-rs/rsql@776f4da47fc97ba78ca0c63656a8ee770d6a1ad9 🚀

---
+++ rsql_cli/book/chapter2/locale/demo.sql
@@ -0,0 +1,270 @@
+.echo prompt
+.help
+.locale ar
+
+.صدى إيقاف
+.نوم 1
+.صدى موجه
+.مساعدة
+.اللغة be
+
+.адлюстраваць выключана
+.спаць 1
+.адлюстраваць промпт
+.дапамога
+.месцазнаходжанне bg
+
+.еко изключено
+.спиране 1
+.еко подсказка
+.помощ
+.локал bn
+
+.ইকো বন্ধ
+.নিদ্রা 1
+.ইকো প্রম্পট
+.সাহায্য
+.লোকেল cs
+
+.ozvěna vypnuto
+.spát 1
+.ozvěna nápověda
+.pomoc
+.lokalizace da
+
+.ekko fra
+.sove 1
+.ekko hurtig
+.hjælp
+.lokalitet de
+
+.echo aus
+.schlafen 1
+.echo prompt
+.hilfe
+.lokalisierung el
+
+.ηχώ απενεργό
+.ύπνος 1
+.ηχώ προτροπή
+.βοήθεια
+.τοπικοποίηση es
+
+.eco desactivado
+.dormir 1
+.eco indicación
+.ayuda
+.lugar et
+
+.ekho väljas
+.magama 1
+.ekho käsk
+.abi
+.lokaat fi
+
+.kaiku pois
+.nukkua 1
+.kaiku kehotus
+.apua
+.alue fr
+
+.écho désactivé
+.dormir 1
+.écho invite
+.aide
+.localisation ga
+
+.éisteacht díreach
+.codladh 1
+.éisteacht comhartha
+.cabhair
+.limistéar he
+
+.לישון 1
+.עזרה
+.איזור hi
+
+.ध्वनि बंद
+.सोना 1
+.ध्वनि प्रॉम्प्ट
+.मदद
+.स्थानीय hr
+
+.odjek isključeno
+.spavati 1
+.odjek uputa
+.pomoć
+.lokacija hu
+
+.visszhang ki
+.aludni 1
+.visszhang parancs
+.segítség
+.hely is
+
+.endurhljóð af
+.sofa 1
+.endurhljóð skipun
+.hjálp
+.staðsetning it
+
+.eco disattivo
+.dormire 1
+.eco richiesta
+.aiuto
+.localizzazione ja
+
+.エコー オフ
+.眠る 1
+.エコー プロンプト
+.ヘルプ
+.ロケール jv
+
+.kumandhang mati
+.turu 1
+.kumandhang petunjuk
+.pitulung
+.lokasi ka
+
+.ექო გამორთულია
+.ძილი 1
+.ექო მითითება
+.დახმარება
+.ლოკალი ko
+
+.반향 끔
+.자다 1
+.반향 프롬프트
+.도움말
+.로케일 lt
+
+.atspindys išjungta
+.miegoti 1
+.atspindys nurodymas
+.pagalba
+.vietovė lv
+
+.atspoguļošana izslēgts
+.gulēt 1
+.atspoguļošana norāde
+.palīdzība
+.lokalizācija mk
+
+.еко исклучено
+.спиј 1
+.еко промпт
+.помош
+.локал ms
+
+.gema mati
+.tidur 1
+.gema petunjuk
+.bantuan
+.tempatan mt
+
+.eku le
+.nixfa 1
+.eku indikazzjoni
+.għajnuna
+.lokal nl
+
+.echo uit
+.slapen 1
+.echo prompt
+.hulp
+.plaats no
+
+.ekko av
+.sove 1
+.ekko ledetekst
+.hjelp
+.lokalisering pl
+
+.echo wyłączone
+.spać 1
+.echo polecenie
+.pomoc
+.lokalizacja pt
+
+.eco desligado
+.dormir 1
+.eco prazo
+.ajuda
+.local ro
+
+.ecou oprit
+.somn 1
+.ecou prompt
+.ajutor
+.localizare ru
+
+.эхо выключено
+.спать 1
+.эхо подсказка
+.помощь
+.локализация sk
+
+.ozvena vypnuté
+.spať 1
+.ozvena nápoveda
+.pomoc
+.lokalita sl
+
+.odmev izključeno
+.spanje 1
+.odmev navodilo
+.pomoč
+.lokalizacija sq
+
+.jehonë jashtë
+.flutur 1
+.jehonë udhëzim
+.ndihmë
+.lokale sr
+
+.еко искључено
+.спавати 1
+.еко подсетник
+.pomoć
+.локал sv
+
+.ekon av
+.sova 1
+.ekon prompt
+.hjälp
+.lokal th
+
+.เสียงสะท้อน ปิด
+.นอน 1
+.เสียงสะท้อน คำสั่ง
+.ช่วยเหลือ
+.ภูมิภาค tr
+
+.yankı kapalı
+.uyumak 1
+.yankı komut
+.yardım
+.yerel uk
+
+.еко вимкнено
+.спати 1
+.еко підказка
+.допомога
+.локалізація vi
+
+.tiếng-vọng tắt
+.ngủ 1
+.tiếng-vọng gợi
+.trợ giúp
+.tiếng yi
+
+.הד אויס
+.שלאַפֿן 1
+.הד הוראה
+.הילף
+.לאָקאַלע zh
+
+.回声 关
+.睡觉 1
+.回声 提示
+.帮助
